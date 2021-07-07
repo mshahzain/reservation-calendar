df3713e562f3e118847e4883108a84977ce442d7
Appleiphone3gs10 <ms03977@st.habib.edu.pk>
2021-07-07 20:22:16 +0500

    Calendar done -  I think

--- app/controllers/CalendarController.js
+++ app/controllers/CalendarController.js
@@ -9,25 +9,56 @@ function CalendarController(weekDays)
 {
   var calendar = this;
 
+  calendar.currentMonth='';
+  calendar.currentYear='';
   calendar.weekDays =  weekDays;
   calendar.weeks = 5;
-  calendar.paddedWeeksArr = [7,14,21,27];
+  calendar.paddedWeeksArr = [7 , 14, 21 , 28, 35 ];
   calendar.paddingDays = '';
   calendar.paddingDaysArr = [];
   calendar.NonPaddingDaysArr = [];
   calendar.daysInMonth = '';//initialized
   //assigned value in loadCalendarHelper function;
 
-  var loadCalendarHelper = function()
+  calendar.goToPreviousMonth = function()
   {
+    if(calendar.currentMonth == 0)
+    {
+      var year = Number(calendar.currentYear) - 1;
+      loadCalendarHelper(11, year);
+    }
+    else
+    {
+      loadCalendarHelper( calendar.currentMonth - 1,calendar.currentYear);
+    }
 
+  };
+  calendar.goToNextMonth =  function()
+  {
+    if(calendar.currentMonth == 11)
+    {
+      var year = Number(calendar.currentYear) + 1;
+      loadCalendarHelper(0, year);
+    }
+    else
+    {
+      loadCalendarHelper( calendar.currentMonth + 1,calendar.currentYear);
+    }
+
+  };
+
+  var loadCalendarHelper = function(monthArg, yearArg) //month: 0-11, year: "Year"
+  {
+    calendar.paddingDaysArr = [];
+    calendar.paddingDays = 0;
+    calendar.NonPaddingDaysArr = [];
     const dt = new Date();
     const day = dt.getDate();
     const month = dt.getMonth();
     const year = dt.getFullYear();
 
-    const firstDayOfMonth = new Date("2020", month, 1);
-    const daysInMonth = new Date("2020", month + 1, 0).getDate();
+    const firstDayOfMonth = new Date(yearArg, monthArg, 1);
+    const daysInMonth = new Date(yearArg, monthArg + 1, 0).getDate();
     // 0th day as the third parameter gives the last day of the previous month.
     // That is the what we need to calculate total days in the current month.
     console.log(daysInMonth);
@@ -36,11 +67,15 @@ function CalendarController(weekDays)
     const dateString =  firstDayOfMonth.toLocaleDateString('en-us',{
       weekday: 'long',
       year: 'numeric',
-      month: 'numeric',
+      month: 'short',
       day: 'numeric',
     });
 
-    calendar.paddingDays = Number(weekDays.indexOf(dateString.split(', ')[0]));
+    let dateStringArr = dateString.split(', ');
+    console.log(dateStringArr);
+    calendar.month = dateStringArr[1];
+    calendar.paddingDays = Number(weekDays.indexOf(dateStringArr[0]));
+
     calendar.daysInMonth = Number(daysInMonth);
 
     //dateString.split() gets the string separated by commas
@@ -56,7 +91,10 @@ function CalendarController(weekDays)
       calendar.NonPaddingDaysArr.push(i);
     }
 
+    calendar.currentMonth = monthArg;
+    calendar.currentYear = yearArg;
+
   }
-  loadCalendarHelper();
+  loadCalendarHelper(6,"2020");
 
 }
